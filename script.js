e62f3a29618cbf83385971132512b336ae30bb55
solszym <solszym@gmail.com>
2023-03-13 23:34:37 +0100

    newBook update

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const content = document.getElementById("content");
+const library = document.getElementById("library");
+const addBookBtn = document.querySelector("[data-modal-target]");
+const overlay = document.getElementById("overlay");
+
+addBookBtn.addEventListener("click", () => {
+  const modal = document.querySelector(addBookBtn.dataset.modalTarget);
+  openModal(modal);
+});
+
+overlay.addEventListener("click", () => {
+  const modal = document.querySelector("#newBookWindow.active");
+  closeModal(modal);
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 let myLibrary = [];
 
